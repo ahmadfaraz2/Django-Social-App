d566b103c2130a1c3ff333f0519f2911f22167df
Ahmad Faraz <ahmadfarazjanjua789@gmail.com>
2025-02-01 23:14:48 +0500

    Use Django auth views to setup password reset functionality, made custom templates and add styling

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -126,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email configs
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
